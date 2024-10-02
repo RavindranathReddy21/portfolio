9ac2ef2c9284ad2e89f5ae3f81616839418ba716
RavindranathReddy21 <ravindranath.thor@gmail.com>
2024-10-02 19:54:16 +0530

    small change to leetcode card

--- src/components/LeetCodeCard.jsx
+++ src/components/LeetCodeCard.jsx
@@ -4,7 +4,7 @@ const LeetCodeCard = () => {
   return (
     <div className="w-full flex justify-center">
       <iframe
-        src="https://leetcard.jacoblin.cool/ravindranath_21?theme=dark&font=Hind%20Guntur&ext=heatmap"
+        src="https://leetcard.jacoblin.cool/ravindranath_21?theme=dark&font=Hind%20Guntur&ext=contest"
         className="w-full aspect-video bg-inherit rounded-[50px]"
         title="LeetCode Card"
       ></iframe>
